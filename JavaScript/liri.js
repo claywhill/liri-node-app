65c5191faa156db780a5f16ecfd0b55f708fd6fe
Clayton Hill <clayman0123@yahoo.com>
2020-04-04 15:59:34 -0600

    built working command interface

--- JavaScript/liri.js
+++ JavaScript/liri.js
@@ -1,16 +1,35 @@
-require('dotenv').config();
+require("dotenv").config();
 
-var keys = require('./keys.js');
-
-var Spotify = require('node-spotify-api');
-
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+var cmd = process.argv[2];
+var query = process.argv.slice(3).join(" ");
 
-var spotifyThisSong = process.argv[2];
- 
-spotify.search({ type: "track", query: spotifyThisSong }, function (err, data) {
+function searchSpotify(value) {
+  spotify.search({ type: "track", query: value }, function (err, data) {
     if (err) {
-        return console.log('Error occurred: ' + err);
+      return console.log("Error occurred: " + err);
     }
     console.log(data.tracks.items[2]);
-});
+  });
+}
+
+if (query === "") {
+    console.log("Please enter a song.");
+    
+} else if (cmd === "spotify-this-song") {
+  searchSpotify(query);
+
+} else if (cmd === "concert-this") {
+    // search Bands In Town API;
+
+} else if (cmd === "movie-this") {
+    // search OMDB API;
+
+} else if (cmd === "do-what-it-says") {
+    // do what is says
+
+} else {
+    console.log(cmd + " is not a valid command. Please enter a valid command.")
+};
